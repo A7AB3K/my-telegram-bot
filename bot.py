fbdc344a486679b13079c1445cbae3b45d599ec0
A7AB3K <atabekjoldasbaev888@gmail.com>
2026-05-10 16:05:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,19 @@
 import os
-import google.generativeai as genai
+from google import genai
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
-    response = model.generate_content(user_message)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=user_message
+    )
     await update.message.reply_text(response.text)
 
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
